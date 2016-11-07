a3cfa93586ffc689e5dfb43389fabafd3558c101
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-11-07 09:14:23 -0600

    v410: inherit embedded makefile for plasma-mobile
    Change-Id: I6152d81056771096e16df8fd52cad9f047f93cd2

--- full_v410.mk
+++ full_v410.mk
@@ -15,7 +15,7 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
 
 # Inherit from v410 device
 $(call inherit-product, device/lge/v410/device.mk)
